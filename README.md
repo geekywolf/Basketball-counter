5a3a46c6cde71431cb1e3e5cedfc1939cda3b0f1
Tee <t4riumph@gmail.com>
2022-08-15 12:10:45 +0100

    added start buton and js functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Basketball-counter
 
 This is my first full project with javascript
+
+figma design : https://www.figma.com/file/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=0%3A1
--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,66 @@ let guestScore = document.getElementById('guestScore')
 let homeResult = 0
 let guestResult = 0
 
+
+function scoretest(){
+        
+    if (homeResult > guestResult){
+
+        homeScore.style.color = "#FFF";
+        guestScore.style.color = "#F94F6D"
+
+    }else if(guestResult > homeResult){
+
+        guestScore.style.color = "#FFF";
+        homeScore.style.color = "#F94F6D";
+
+    }else{
+        homeScore.style.color = "#F94F6D";
+        guestScore.style.color = "#F94F6D";
+    }
+
+}
+
 function homeScorePlus1(){
     homeResult += 1
     homeScore.innerText = homeResult
+    scoretest()
 }
 
 function homeScorePlus2(){
     homeResult += 2
     homeScore.innerText = homeResult
+    scoretest()
 }
 
 function homeScorePlus3(){
     homeResult += 3
     homeScore.innerText = homeResult
+    scoretest()
 }
 
 function guestScorePlus1(){
     guestResult += 1
     guestScore.innerText = guestResult
+    scoretest()
 }
 
 function guestScorePlus2(){
     guestResult += 2
     guestScore.innerText = guestResult
+    scoretest()
 }
 
 function guestScorePlus3(){
     guestResult += 3
     guestScore.innerText = guestResult
+    scoretest()
+}
+
+function restart(){
+    homeResult = 0
+    guestResult = 0
+    homeScore.innerText = homeResult
+    guestScore.innerText = guestResult
+    scoretest()
 }
